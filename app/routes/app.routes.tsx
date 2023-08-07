2d468952ecad6d453d5d894f6d2197bda97a5f7b
pedro <dev.pedro.nogueira@gmail.com>
2023-08-07 19:55:30 -0300

    bottom tab styling + type

--- app/routes/app.routes.tsx
+++ app/routes/app.routes.tsx
@@ -1,18 +1,97 @@
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import {
+  BottomTabBarButtonProps,
+  BottomTabNavigationProp,
+  createBottomTabNavigator,
+} from "@react-navigation/bottom-tabs";
 import { Home } from "../screens/Home";
 import { AboutUs } from "../screens/AboutUs";
 import { Schedules } from "../screens/Schedules";
 import { Profile } from "../screens/Profile";
+import {
+  Buildings,
+  Calendar,
+  House,
+  User,
+  Users,
+  WhatsappLogo,
+} from "phosphor-react-native";
+import { Platform } from "react-native";
 
-const { Navigator, Screen } = createBottomTabNavigator();
+type AppRoutes = {
+  home: undefined;
+  aboutUs: undefined;
+  schedules: undefined;
+  profile: undefined;
+  whatsapp: undefined;
+};
+
+export type AppNavigatorRoutesProps = BottomTabNavigationProp<AppRoutes>;
+
+const { Navigator, Screen } = createBottomTabNavigator<AppRoutes>();
 
 export const AppRoutes = () => {
   return (
-    <Navigator>
-      <Screen name="home" component={Home}></Screen>
-      <Screen name="aboutUs" component={AboutUs}></Screen>
-      <Screen name="schedules" component={Schedules}></Screen>
-      <Screen name="profile" component={Profile}></Screen>
+    <Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: "#826B12",
+        tabBarStyle: {
+          paddingBottom: 12,
+          paddingTop: 16,
+          height: 58,
+        },
+      }}
+    >
+      <Screen
+        name="home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ size, color }) => {
+            return <House color={color} size={28} />;
+          },
+        }}
+      ></Screen>
+
+      <Screen
+        name="aboutUs"
+        component={AboutUs}
+        options={{
+          tabBarIcon: ({ size, color }) => {
+            return <Buildings color={color} size={28} />;
+          },
+        }}
+      ></Screen>
+
+      <Screen
+        name="schedules"
+        component={Schedules}
+        options={{
+          tabBarIcon: ({ size, color }) => {
+            return <Calendar color={color} size={28} />;
+          },
+        }}
+      ></Screen>
+
+      <Screen
+        name="profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({ size, color }) => {
+            return <User color={color} size={28} />;
+          },
+        }}
+      ></Screen>
+
+      <Screen
+        name="whatsapp"
+        component={Profile}
+        options={{
+          tabBarIcon: ({ size, color }) => {
+            return <WhatsappLogo color={color} size={28} />;
+          },
+        }}
+      ></Screen>
     </Navigator>
   );
 };
