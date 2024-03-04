49c0b6a09f62c6fcc03dd76a857f339ed414e8d6
Pedro Soares <dev.pedro.nogueira@gmail.com>
2024-03-04 16:21:22 -0300

    icons map

--- app/components/NewAreas.tsx
+++ app/components/NewAreas.tsx
@@ -1,6 +1,6 @@
-import { Modal, Stack, Text } from "native-base";
+import { Box, Modal, Stack, Text } from "native-base";
 import React, { useState } from "react";
-import { TouchableOpacity } from "react-native";
+import { Linking, TouchableOpacity } from "react-native";
 import { IAreas } from "../reducers/fetchSlice";
 
 import DireitoTrabalho from "../assets/areas_de_atuacao_icons/Direito do Trabalho.svg";
@@ -13,19 +13,41 @@ import DireitoFamília from "../assets/areas_de_atuacao_icons/Direito de Famíli
 import Trânsito from "../assets/areas_de_atuacao_icons/Trânsito.svg";
 
 const iconsMap = {
-  Administrativo: DireitoAdministrativo,
-  "Administrativo Previdenciário": DireitoPrevidenciario,
-  Civel: DireitoTributário,
-  Previdenciário: DireitoTrabalho,
-  Trabalhista: DireitoDigital,
-  Tributário: DireitoTributário,
-  Outros: DireitoFamília,
+  Administrativo: {
+    icon: DireitoAdministrativo,
+    url: "O Direito Administrativo é uma área bastante ampla e atuamos nas suas mais variadas vertentes.",
+  },
+  "Administrativo Previdenciário": { icon: DireitoPrevidenciario, url: "" },
+  Cível: {
+    icon: DireitoFamília,
+    url: "O Direito Civil é a área mais abrangente do Direito Brasileiro e nosso escritório está preparado para atuar em suas mais variadas vertentes.",
+  },
+  Previdenciário: {
+    icon: DireitoTrabalho,
+    url: "Atuamos no ramo do Direito Previdenciário há quase 40 anos e possuímos uma equipe altamente qualificada neste segmento, buscando sempre assegurar aos nossos clientes a percepção do melhor benefício previdenciário a que tem direito.",
+  },
+  Trabalhista: {
+    icon: DireitoDigital,
+    url: "Direito do trabalho é o ramo jurídico que estuda as relações de trabalho. Esse direito é composto de conjuntos de normas, princípios e outras fontes jurídicas que regem as relações de trabalho, regulamentando a condição jurídica dos trabalhadores.",
+  },
+  Tributário: {
+    icon: DireitoTributário,
+    url: "Diagnosticar as necessidades de nosso cliente e aplicar a legislação tributária de modo mais benéfico ao crescimento de seus negócios é o principal objetivo de nossa equipe tributária.",
+  },
+  Outros: {
+    icon: DireitoTributário,
+    url: "Diante de um mundo cada vez mais tecnológico que vem revolucionando o modo como nos relacionamos e fazemos negócios, necessária foi a criação de um novo ramo no Direito que tratasse especificamente sobre essas relações jurídicas.",
+  },
 };
 
 const NewAreas = (props: IAreas) => {
   const [showModal, setShowModal] = useState(false);
 
-  const IconComponent = iconsMap[props.Nome];
+  const IconComponent = iconsMap[props.Nome].icon;
+
+  const openExternalLink = (url: string) => {
+    Linking.openURL(url);
+  };
 
   return (
     <>
@@ -51,6 +73,21 @@ const NewAreas = (props: IAreas) => {
               <Text className="text-xl font-bold text-zinc-800">
                 36 ANOS DE EXPERIÊNCIA
               </Text>
+
+              <Box>
+                {/* <TouchableOpacity
+                  onPress={() => openExternalLink(iconsMap[props.Nome].url)}
+                  className="flex w-full items-center justify-between gap-0.5 rounded-md bg-yellow-100 px-4 py-4"
+                >
+                  <Text className="text-center ">
+                    Acesse nosso site para mais informações. Clique aqui
+                  </Text>
+                </TouchableOpacity> */}
+
+                <Text className="mt-10 text-lg font-normal">
+                  {iconsMap[props.Nome].url}
+                </Text>
+              </Box>
             </Stack>
           </Modal.Body>
         </Modal.Content>
